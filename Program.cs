fa3d3d13a141d2706df5858958c58fd4a6f6ff0d
hnguyen4438 <98571207+hnguyen4438@users.noreply.github.com>
2022-03-07 21:03:17 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+//Name: Hieu Nguyen
+
+using System;
 
 namespace Tech_Assignment__3_Using_Iterative_Statenent
 {
@@ -8,18 +10,22 @@ namespace Tech_Assignment__3_Using_Iterative_Statenent
         {
             double CAD = 0, USD;
 
+    //Get the input - CAD incremental value
             Console.WriteLine("Enter a CAD incremental value");
 
             try
             {
                 string inputdata = Console.ReadLine();
                 int inputvalue = int.Parse(inputdata);
+    //Use conditional if/else statement to check the value that user enter
+    //If input < 5 or input > 25: try again
                 if (inputvalue < 5 || inputvalue > 25)
                 {
                     Console.WriteLine("Try again with value between 5 and 25");
                     Console.WriteLine("Press any keys to exit the program");
                     Console.ReadKey(true);
                 }
+     //If put between 5 to 25, perform calculation until CAD value reach 200
                 else
                 {
                     Console.WriteLine("{0,-10:C} {1,-10:C}", "CAD", "US$");
